4a909325c148f8fa37d16501fefeed9fa43da87b
9631kunn <infourbh+1@gmail.com>
2020-07-20 22:48:42 +0000

    fix: environment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,4 @@
-const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
-
-
-
-require('dotenv').config();
+require('dotenv').config(); 
 
 module.exports = {
   siteMetadata: {
@@ -37,13 +33,13 @@ module.exports = {
       resolve: 'gatsby-theme-firebase',
       options: {
         credentials: {
-          apiKey: process.env.FIREBASE_API_KEY,
-          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-          databaseURL: process.env.FIREBASE_DATABASE_URL,
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-          appId: process.env.FIREBASE_APP_ID,
+          apiKey: GATSBY_FIREBASE_API_KEY,
+          authDomain: GATSBY_FIREBASE_AUTH_DOMAIN,
+          databaseURL: GATSBY_FIREBASE_DATABASE_URL,
+          projectId: GATSBY_FIREBASE_PROJECT_ID,
+          storageBucket: GATSBY_FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+          appId: GATSBY_FIREBASE_APP_ID,
         },
         loginPath: '/login',
         loginRedirectPath: '/dashboard',
